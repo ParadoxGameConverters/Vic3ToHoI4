e2d9c918cdc41d6bef91e64961a6e51beacc316f
Stephen May <idhrendur@hotmail.com>
2022-08-25 22:16:05 -0700

    Fix codacy warning

--- tools/build_fronter.sh
+++ tools/build_fronter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # to be run from the project root directory
-cd external/Fronter.NET
+cd external/Fronter.NET || exit
 dotnet build --configuration=release
 cp -r Release/* ../../build/Release-Linux
