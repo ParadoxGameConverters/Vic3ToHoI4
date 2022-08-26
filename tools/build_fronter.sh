25f4a97142ac69e2d1224168374c3b102b11fc96
Stephen May <idhrendur@hotmail.com>
2022-08-25 22:18:53 -0700

    Try to fix Fronter build

--- tools/build_fronter.sh
+++ tools/build_fronter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # to be run from the project root directory
 cd external/Fronter.NET || exit
+dotnet restore
 dotnet build --configuration=release
 cp -r Release/* ../../build/Release-Linux
