85b35f20c702b0165dd3225cf769884b33109b92
Stephen May <idhrendur@hotmail.com>
2022-09-23 18:56:12 -0700

    Fix main build script

--- tools/build_vic3_to_hoi4.sh
+++ tools/build_vic3_to_hoi4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -H. -Bbuild && cmake --build ./build -- -j40 && cd build/test/Release-Linux && ./Vic3ToHoI4Tests && cd ..
+cmake -H. -Bbuild && cmake --build ./build --target Vic3ToHoi4 Fronter Vic3ToHoi4Tests -- -j40 && cd build/test/Release-Linux && ./Vic3ToHoI4Tests && cd ..
