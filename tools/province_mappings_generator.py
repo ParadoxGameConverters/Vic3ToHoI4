a6062b80d455c29a102ce001c35a5f9ce85f01fb
Stephen May <idhrendur@hotmail.com>
2022-10-25 17:33:41 -0700

    Add template for province mappings

---
+++ tools/province_mappings_generator.py
@@ -0,0 +1,31 @@
+import os
+# assign directory
+directory = '/mnt/c/Program Files (x86)/Steam/steamapps/common/Victoria 3/game/map_data/state_regions'
+
+print('0.0.0 = {')
+
+for filename in os.listdir(directory):
+  filename = os.path.join(directory, filename)
+  if os.path.isfile(filename):
+    file_string = os.path.splitext(os.path.basename(filename))[0][3:]
+    print('\tlink = { comment = "* **********" }')
+    print('\tlink = { comment = ' + file_string + ' }')
+    print('\tlink = { comment = "* **********" }')
+    print('\tlink = { comment = "* " }')
+
+    file = open(filename)
+    bracket_level = 0
+    for line in file:
+      if not line:
+        continue
+      if line == '\n':
+        continue
+      if bracket_level == 0:
+        print('\tlink = { comment = "* * ' + line[:-4] + '" }')
+      if '{' in line:
+        bracket_level += 1
+      if '}' in line:
+        bracket_level -= 1
+
+    print('\tlink = { comment = "* " }')
+print('}')
