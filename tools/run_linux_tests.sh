7c11cb5363788be775cf0dbde242f3866ab7ae90
Idhrendur <idhrendur@hotmail.com>
2022-10-29 11:24:32 -0700

    Add a script to run linux tests

---
+++ tools/run_linux_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm build/test/Release-Linux/Vic3ToHoi4Tests
+cmake -H. -Bbuild && cmake --build ./build --target Vic3ToHoi4 Vic3ToHoi4Tests -- -j40
+(
+cd build/test/Release-Linux
+./Vic3ToHoi4Tests
+)
