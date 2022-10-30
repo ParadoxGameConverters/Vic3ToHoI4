fa8024bd41060c0dadc3664415632448dcf36858
Idhrendur <idhrendur@hotmail.com>
2022-10-30 16:47:05 -0700

    Codacy

--- src/vic3_world/states/state_importer.cpp
+++ src/vic3_world/states/state_importer.cpp
@@ -14,15 +14,15 @@ vic3::StateImporter::StateImporter()
 
    provinces_parser_.registerKeyword("provinces", [this](std::istream& input_stream) {
       const auto provinces_input = commonItems::getInts(input_stream);
-      if (provinces_input.size() % 2 != 0)
+      if ((provinces_input.size() % 2) != 0)
       {
          throw std::runtime_error(
              "Provinces entry was malformed. Please go to the forum thread and upload log.txt and your save.");
       }
-      for (unsigned int i = 0; i < provinces_input.size(); i += 2)
+      for (unsigned int i = 0; i < provinces_input.size(); i += 2U)
       {
          const int start_province = provinces_input[i];
-         const int additional_provinces = provinces_input[i + 1];
+         const int additional_provinces = provinces_input[i + 1U];
          for (int j = 0; j <= additional_provinces; j++)
          {
             provinces_.insert(start_province + j);
