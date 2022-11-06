8ba8f97e6c39ebed7536755ff8b2516d5a8fb2bb
Stephen May <idhrendur@hotmail.com>
2022-11-06 13:13:00 -0800

    Get more data during build

--- tools/build_vic3_to_hoi4.sh
+++ tools/build_vic3_to_hoi4.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cmake -H. -Bbuild && cmake --build ./build --target Vic3ToHoi4 Fronter Vic3ToHoi4Tests -- -j40 && cd build/test/Release-Linux && ./Vic3ToHoI4Tests && cd ..
+cmake -H. -Bbuild && cmake --build ./build --target Vic3ToHoi4 Fronter Vic3ToHoi4Tests -- -j40 && cd build/test/Release-Linux
+pwd
+ls
+./Vic3ToHoI4Tests && cd ..
