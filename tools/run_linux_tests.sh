5ace42e58ad5c402f14a799b87116804db23b37f
Stephen May <idhrendur@hotmail.com>
2022-11-13 11:34:46 -0800

    Update a script to make codacy happier

--- tools/run_linux_tests.sh
+++ tools/run_linux_tests.sh
@@ -2,6 +2,6 @@
 rm build/test/Release-Linux/Vic3ToHoi4Tests
 cmake -H. -Bbuild && cmake --build ./build --target Vic3ToHoi4 Vic3ToHoi4Tests -- -j40
 (
-cd build/test/Release-Linux
+cd build/test/Release-Linux || exit
 ./Vic3ToHoi4Tests
 )
