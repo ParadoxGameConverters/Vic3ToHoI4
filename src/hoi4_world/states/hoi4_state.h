8858ecc5d888e4819f4f14f91af9688400e3d0de
Idhrendur <idhrendur@hotmail.com>
2022-11-19 00:46:36 -0800

    Add a class for a hoi4 state and the means to output it.

---
+++ src/hoi4_world/states/hoi4_state.h
@@ -0,0 +1,30 @@
+#ifndef SRC_HOI4WORLD_STATES_HOI4STATE_H
+#define SRC_HOI4WORLD_STATES_HOI4STATE_H
+
+
+
+#include <set>
+
+
+
+namespace hoi4
+{
+
+class State
+{
+  public:
+   explicit State(int id, std::set<int> provinces): id_(id), provinces_(std::move(provinces)) {}
+
+   [[nodiscard]] int GetId() const { return id_; }
+   [[nodiscard]] const std::set<int>& GetProvinces() const { return provinces_; }
+
+  private:
+   int id_;
+   std::set<int> provinces_;
+};
+
+}  // namespace hoi4
+
+
+
+#endif  // SRC_HOI4WORLD_STATES_HOI4STATE_H
