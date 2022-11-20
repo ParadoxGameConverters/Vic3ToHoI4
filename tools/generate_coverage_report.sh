2970904b11814d8c874ea7c6a801bd548529cc7b
Idhrendur <idhrendur@hotmail.com>
2022-11-20 13:21:13 -0800

    Improvements to coverage generation (#168)

--- tools/generate_coverage_report.sh
+++ tools/generate_coverage_report.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-rm -rf build/
+
+rm -rf build/CMakeFiles
+rm -rf build/gcoverage
+rm -rf build/lcoverage
+
 cmake -H. -Bbuild -DCOVERAGE=TRUE && cmake --build ./build --target Vic3ToHoi4 Vic3ToHoi4Tests -- -j40
+
 cd build || exit
 cd test/Release-Linux || exit
 ./Vic3ToHoi4Tests
 cd ../.. || exit
+
 make gcov
 make lcov
