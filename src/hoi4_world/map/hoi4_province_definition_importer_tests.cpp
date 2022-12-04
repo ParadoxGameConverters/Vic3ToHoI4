dbebc392f76430b08c307f1225846d1e01c6ac56
Idhrendur <idhrendur@hotmail.com>
2022-12-04 10:32:37 -0800

    Import hoi4 province definitions.

---
+++ src/hoi4_world/map/hoi4_province_definition_importer_tests.cpp
@@ -0,0 +1,33 @@
+#include "external/googletest/googlemock/include/gmock/gmock-matchers.h"
+#include "external/googletest/googletest/include/gtest/gtest.h"
+#include "src/hoi4_world/map/hoi4_province_definition_importer.h"
+
+
+
+TEST(Hoi4worldMapHoi4provincedefinitionimporter, ExceptionThrownForNoDefinitions)
+{
+   commonItems::ModFilesystem mod_filesystem("", {});
+
+   EXPECT_THROW(const maps::ProvinceDefinitions province_definitions = hoi4::ImportProvinceDefinitions(mod_filesystem),
+       std::runtime_error);
+}
+
+
+TEST(Hoi4worldMapHoi4provincedefinitionimporter, DefinitionsCanBeImported)
+{
+   commonItems::ModFilesystem mod_filesystem("test_files/hoi4_world", {});
+
+   const maps::ProvinceDefinitions province_definitions = hoi4::ImportProvinceDefinitions(mod_filesystem);
+
+   EXPECT_THAT(province_definitions.GetLandProvinces(), testing::UnorderedElementsAre("2"));
+}
+
+
+TEST(Hoi4worldMapHoi4provincedefinitionimporter, SeaProvincesAreDetected)
+{
+   commonItems::ModFilesystem mod_filesystem("test_files/hoi4_world", {});
+
+   const maps::ProvinceDefinitions province_definitions = hoi4::ImportProvinceDefinitions(mod_filesystem);
+
+   EXPECT_TRUE(province_definitions.IsSeaProvince("10"));
+}
