6169b855b97f31820ff2b3fd4c2566defa0ef6f6
Idhrendur <idhrendur@hotmail.com>
2022-12-24 00:43:47 -0800

    Create country mappings for all vic3 countries.

---
+++ src/mappers/country/country_mapper_creator.cpp
@@ -0,0 +1,30 @@
+#include "src/mappers/country/country_mapper_creator.h"
+
+#include <ranges>
+
+#include "external/fmt/include/fmt/format.h"
+
+
+
+mappers::CountryMapper mappers::CreateCountryMappings(const std::map<int, vic3::Country>& countries)
+{
+   std::map<std::string, std::string> country_mappings;
+
+   char tag_prefix = 'Z';
+   int tag_suffix = 0;
+
+   for (const vic3::Country& country: countries | std::views::values)
+   {
+      const auto& tag = country.GetTag();
+      country_mappings.emplace(tag, fmt::format("{}{:0>2}", tag_prefix, tag_suffix));
+
+      ++tag_suffix;
+      if (tag_suffix > 99)
+      {
+         tag_suffix = 0;
+         --tag_prefix;
+      }
+   }
+
+   return CountryMapper(country_mappings);
+}
