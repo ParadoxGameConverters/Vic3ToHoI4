d3bb1cefece43e6c11b5b092916569477c91cc40
Idhrendur <idhrendur@hotmail.com>
2022-12-24 10:54:06 -0800

    Add tests

---
+++ src/mappers/country/country_mapper_creator_tests.cpp
@@ -0,0 +1,47 @@
+#include <sstream>
+
+#include "external/fmt/include/fmt/format.h"
+#include "external/googletest/googlemock/include/gmock/gmock-matchers.h"
+#include "external/googletest/googletest/include/gtest/gtest.h"
+#include "src/mappers/country/country_mapper.h"
+#include "src/mappers/country/country_mapper_creator.h"
+
+
+
+namespace mappers
+{
+
+TEST(MappersCountryCountryMapperCreator, GeneratedMappingsCanBeCreated)
+{
+   std::map<int, vic3::Country> source_countries{{1, vic3::Country({.tag = "000"})}};
+
+   const CountryMapper country_mapper = CreateCountryMappings(source_countries);
+   EXPECT_EQ(country_mapper.GetHoiTag("000"), "Z00");
+}
+
+
+TEST(MappersCountryCountryMapperCreator, GeneratedMappingsIncrementTheNumericDigits)
+{
+   std::map<int, vic3::Country> source_countries{{1, vic3::Country({.tag = "000"})},
+       {2, vic3::Country({.tag = "001"})}};
+
+   const CountryMapper country_mapper = CreateCountryMappings(source_countries);
+   EXPECT_EQ(country_mapper.GetHoiTag("000"), "Z00");
+   EXPECT_EQ(country_mapper.GetHoiTag("001"), "Z01");
+}
+
+TEST(MappersCountryCountryMapperCreator, GeneratedMappingsDecrementTheAlphaWhenTheNumericsRollOver)
+{
+   std::map<int, vic3::Country> source_countries;
+   for (int i = 0; i < 102; ++i)
+   {
+      source_countries.emplace(i, vic3::Country({.tag = fmt::format("{:0>3}", i)}));
+   }
+
+   const CountryMapper country_mapper = CreateCountryMappings(source_countries);
+   EXPECT_EQ(country_mapper.GetHoiTag("099"), "Z99");
+   EXPECT_EQ(country_mapper.GetHoiTag("100"), "Y00");
+   EXPECT_EQ(country_mapper.GetHoiTag("101"), "Y01");
+}
+
+}  // namespace mappers
