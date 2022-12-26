0c6e500388f35a4626fe696c72d36d03ee3eaf35
Stephen May <idhrendur@hotmail.com>
2022-12-26 14:18:41 -0800

    Use noew fronter build dir

--- tools/build_fronter.sh
+++ tools/build_fronter.sh
@@ -3,6 +3,4 @@
 cd external/Fronter.NET || exit
 dotnet restore
 dotnet build --configuration=release
-pwd
-ls
-cp -r Release/* ../../build/Release-Linux
+cp -r ../Release/* ../../build/Release-Linux
