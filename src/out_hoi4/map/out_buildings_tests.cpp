f0c317153ee5e3c19e7491ab8ebb494aca45e3b0
Idhrendur <idhrendur@hotmail.com>
2023-01-09 20:06:57 -0800

    Tests for out_buildings

--- src/out_hoi4/map/out_buildings_tests.cpp
+++ src/out_hoi4/map/out_buildings_tests.cpp
@@ -6,8 +6,8 @@
 #include "external/fmt/include/fmt/format.h"
 #include "external/googletest/googlemock/include/gmock/gmock-matchers.h"
 #include "external/googletest/googletest/include/gtest/gtest.h"
-#include "src/hoi4_world/map/hoi4_building.h"
-#include "src/out_hoi4/map/out_building.h"
+#include "src/hoi4_world/map/buildings.h"
+#include "src/out_hoi4/map/out_buildings.h"
 
 
 
@@ -15,9 +15,90 @@ namespace out
 {
 
 
-TEST(Outhoi4MapBuildingsTests, stub)
+TEST(Outhoi4MapBuildingsTests, ExceptionForBadPath)
 {
-   EXPECT_TRUE(false);
+   EXPECT_THROW(OutputBuildings("ExceptionForBadPath", hoi4::Buildings({}, {})), std::runtime_error);
+}
+
+
+TEST(Outhoi4MapBuildingsTests, FilesAreCreated)
+{
+   commonItems::DeleteFolder("output/FilesAreCreated");
+   commonItems::TryCreateFolder("output");
+   commonItems::TryCreateFolder("output/FilesAreCreated");
+   commonItems::TryCreateFolder("output/FilesAreCreated/map");
+
+   OutputBuildings("FilesAreCreated", hoi4::Buildings({}, {}));
+
+   EXPECT_TRUE(commonItems::DoesFileExist("output/FilesAreCreated/map/buildings.txt"));
+   EXPECT_TRUE(commonItems::DoesFileExist("output/FilesAreCreated/map/airports.txt"));
+}
+
+
+TEST(Outhoi4MapBuildingsTests, BuildingsAreOutput)
+{
+   commonItems::DeleteFolder("output/BuildingsAreOutput");
+   commonItems::TryCreateFolder("output");
+   commonItems::TryCreateFolder("output/BuildingsAreOutput");
+   commonItems::TryCreateFolder("output/BuildingsAreOutput/map");
+
+   OutputBuildings("BuildingsAreOutput",
+       hoi4::Buildings(
+           {{1,
+                hoi4::Building{.state_id = 42,
+                    .type = "test_type",
+                    .position =
+                        {.x_coordinate = 4.25, .y_coordinate = 9.25, .z_coordinate = 16.25, .rotation = 25.25}}},
+               {1,
+                   hoi4::Building{.state_id = 144,
+                       .type = "second_type",
+                       .position =
+                           {.x_coordinate = 4.25, .y_coordinate = 9.25, .z_coordinate = 16.25, .rotation = 25.25},
+                       .connecting_sea_province = 145}},
+               {2,
+                   hoi4::Building{.state_id = 169,
+                       .type = "third_type",
+                       .position =
+                           {.x_coordinate = 4.25, .y_coordinate = 9.25, .z_coordinate = 16.25, .rotation = 25.25},
+                       .connecting_sea_province = 170}}},
+           {}));
+
+   ASSERT_TRUE(commonItems::DoesFileExist("output/BuildingsAreOutput/map/buildings.txt"));
+   std::ifstream buildings_file("output/BuildingsAreOutput/map/buildings.txt");
+   ASSERT_TRUE(buildings_file.is_open());
+   std::stringstream buildings_file_stream;
+   std::copy(std::istreambuf_iterator<char>(buildings_file),
+       std::istreambuf_iterator<char>(),
+       std::ostreambuf_iterator<char>(buildings_file_stream));
+   buildings_file.close();
+   EXPECT_EQ(buildings_file_stream.str(),
+       "42;test_type;4.25;9.25;16.25;25.25;0\n"
+       "144;second_type;4.25;9.25;16.25;25.25;145\n"
+       "169;third_type;4.25;9.25;16.25;25.25;170\n");
+}
+
+
+TEST(Outhoi4MapBuildingsTests, AirportsAreOutput)
+{
+   commonItems::DeleteFolder("output/AirportsAreOutput");
+   commonItems::TryCreateFolder("output");
+   commonItems::TryCreateFolder("output/AirportsAreOutput");
+   commonItems::TryCreateFolder("output/AirportsAreOutput/map");
+
+   OutputBuildings("AirportsAreOutput", hoi4::Buildings({}, {{2, 4}, {3, 9}, {4, 16}}));
+
+   ASSERT_TRUE(commonItems::DoesFileExist("output/AirportsAreOutput/map/airports.txt"));
+   std::ifstream airports_file("output/AirportsAreOutput/map/airports.txt");
+   ASSERT_TRUE(airports_file.is_open());
+   std::stringstream airports_file_stream;
+   std::copy(std::istreambuf_iterator<char>(airports_file),
+       std::istreambuf_iterator<char>(),
+       std::ostreambuf_iterator<char>(airports_file_stream));
+   airports_file.close();
+   EXPECT_EQ(airports_file_stream.str(),
+       "2={4 }\n"
+       "3={9 }\n"
+       "4={16 }\n");
 }
 
 }  // namespace out
