7e0d39ae0c18d3606c4b23f30a3f10611b4c2505
Zemurin <ivo@element.hr>
2023-02-05 10:00:06 +0100

    codacy

--- tools/build_vic3_to_hoi4.sh
+++ tools/build_vic3_to_hoi4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cmake -H. -Bbuild && \ 
+cmake -H. -Bbuild && \
 cmake --build ./build --target Vic3ToHoi4 ConverterFrontend Vic3ToHoi4Tests -- -j40 && \
 cd build/test/Release-Linux && \
 ./Vic3ToHoi4Tests && \
