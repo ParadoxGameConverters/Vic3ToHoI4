e4a325e0a74f5e8d15bc92b7312147c9377e76c1
Idhrendur <idhrendur@hotmail.com>
2023-02-15 17:58:47 -0800

    Formatting.

--- src/mappers/technology/tech_mappings_importer_tests.cpp
+++ src/mappers/technology/tech_mappings_importer_tests.cpp
@@ -10,9 +10,10 @@ TEST(MappersTechnologyTechmappingsimporterTests, TechMappingsAreImported)
    const std::vector<mappers::TechMapping> tech_mappings = mappers::ImportTechMappings();
 
    EXPECT_THAT(tech_mappings,
-       testing::ElementsAre(
-           mappers::TechMapping{ std::set<std::string>{"source_tech"}, std::nullopt, std::set<std::string>{"dest_tech_one", "dest_tech_two"}},
-           mappers::TechMapping{ std::set<std::string>{"source_tech_two", "source_tech_three"}, std::optional{R"(not = { has_dlc = "Test DLC" })"}, std::set<std::string>{"dest_tech_three"}}
-	)
-   );
+       testing::ElementsAre(mappers::TechMapping{std::set<std::string>{"source_tech"},
+                                std::nullopt,
+                                std::set<std::string>{"dest_tech_one", "dest_tech_two"}},
+           mappers::TechMapping{std::set<std::string>{"source_tech_two", "source_tech_three"},
+               std::optional{R"(not = { has_dlc = "Test DLC" })"},
+               std::set<std::string>{"dest_tech_three"}}));
 }
