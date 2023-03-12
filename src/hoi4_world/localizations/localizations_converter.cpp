327661b9c555b404ba9839b726892bffd282b8a7
Idhrendur <idhrendur@hotmail.com>
2023-03-12 13:15:34 -0700

    Convert state localizations

--- src/hoi4_world/localizations/localizations_converter.cpp
+++ src/hoi4_world/localizations/localizations_converter.cpp
@@ -6,7 +6,11 @@
 
 
 
-hoi4::Localizations hoi4::ConvertLocalizations(const commonItems::LocalizationDatabase& vic3_localizations,
+namespace
+{
+
+commonItems::LocalizationDatabase ConvertCountryLocalizations(
+    const commonItems::LocalizationDatabase& vic3_localizations,
     const std::map<std::string, std::string>& country_mappings)
 {
    commonItems::LocalizationDatabase country_localizations("english",
@@ -34,5 +38,42 @@ hoi4::Localizations hoi4::ConvertLocalizations(const commonItems::LocalizationDa
           *adjective_localization_block);
    }
 
-   return {country_localizations};
+   return country_localizations;
+}
+
+
+commonItems::LocalizationDatabase ConvertStateLocalizations(const commonItems::LocalizationDatabase& vic3_localizations,
+    const std::map<std::string, std::string>& hoi4_state_names_to_vic3_state_names)
+{
+   commonItems::LocalizationDatabase state_localizations("english",
+       {"braz_por", "french", "german", "japanese", "polish", "russian", "spanish"});
+
+   for (const auto& [hoi4_state_name, vic3_state_name]: hoi4_state_names_to_vic3_state_names)
+   {
+      const auto& state_localization_block = vic3_localizations.GetLocalizationBlock(vic3_state_name);
+      if (!state_localization_block)
+      {
+         continue;
+      }
+
+      state_localizations.AddOrModifyLocalizationBlock(hoi4_state_name, *state_localization_block);
+   }
+
+   return state_localizations;
+}
+
+}  // namespace
+
+
+
+hoi4::Localizations hoi4::ConvertLocalizations(const commonItems::LocalizationDatabase& vic3_localizations,
+    const std::map<std::string, std::string>& country_mappings,
+    const std::map<std::string, std::string>& hoi4_state_names_to_vic3_state_names)
+{
+   commonItems::LocalizationDatabase country_localizations =
+       ConvertCountryLocalizations(vic3_localizations, country_mappings);
+   commonItems::LocalizationDatabase state_localizations =
+       ConvertStateLocalizations(vic3_localizations, hoi4_state_names_to_vic3_state_names);
+
+   return {country_localizations, state_localizations};
 }
--- src/hoi4_world/world/hoi4_world_converter.cpp
+++ src/hoi4_world/world/hoi4_world_converter.cpp
@@ -71,8 +71,9 @@ hoi4::World hoi4::ConvertWorld(const commonItems::ModFilesystem& hoi4_mod_filesy
        states.vic3_state_ids_to_hoi4_state_ids,
        tech_mappings);
 
-   Localizations localizations =
-       ConvertLocalizations(source_world.GetLocalizations(), country_mapper.GetCountryMappings());
+   Localizations localizations = ConvertLocalizations(source_world.GetLocalizations(),
+       country_mapper.GetCountryMappings(),
+       states.hoi4_state_names_to_vic3_state_names);
 
    return World(WorldOptions{.countries = countries,
        .states = states,
