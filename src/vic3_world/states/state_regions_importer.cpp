347b3d1743dfb89c409bb8be32f2f89d46c5df55
Idhrendur <idhrendur@hotmail.com>
2023-03-12 11:28:41 -0700

    Import provinces with state regions

--- src/vic3_world/states/state_regions_importer.cpp
+++ src/vic3_world/states/state_regions_importer.cpp
@@ -11,8 +11,21 @@ std::map<std::string, vic3::StateRegion> vic3::ImportStateRegions(const commonIt
    std::map<std::string, vic3::StateRegion> state_regions;
 
    std::map<std::string, std::string> significant_provinces;
+   std::set<std::string> provinces;
 
    commonItems::parser region_parser;
+   region_parser.registerKeyword("provinces", [&provinces](std::istream& input_stream) {
+      for (const auto& province: commonItems::getStrings(input_stream))
+      {
+         std::string transformed_province = province;
+         std::transform(++transformed_province.begin(),
+             transformed_province.end(),
+             ++transformed_province.begin(),
+             ::toupper);
+         transformed_province[0] = 'x';
+         provinces.insert(transformed_province);
+      }
+   });
    region_parser.registerRegex("city|port|farm|mine|wood",
        [&significant_provinces](const std::string& significant_province_type, std::istream& input_stream) {
           std::string province = commonItems::getString(input_stream);
@@ -24,11 +37,12 @@ std::map<std::string, vic3::StateRegion> vic3::ImportStateRegions(const commonIt
 
    commonItems::parser file_parser;
    file_parser.registerRegex(commonItems::catchallRegex,
-       [&state_regions, &region_parser, &significant_provinces](const std::string& region_name,
+       [&state_regions, &region_parser, &significant_provinces, &provinces](const std::string& region_name,
            std::istream& input_stream) {
           significant_provinces.clear();
+          provinces.clear();
           region_parser.parseStream(input_stream);
-          state_regions.emplace(region_name, StateRegion(significant_provinces));
+          state_regions.emplace(region_name, StateRegion(significant_provinces, provinces));
        });
 
    for (const auto& state_regions_file: filesystem.GetAllFilesInFolder("game/map_data/state_regions"))
--- src/vic3_world/states/state_regions_importer_tests.cpp
+++ src/vic3_world/states/state_regions_importer_tests.cpp
@@ -18,29 +18,32 @@ TEST(Vic3worldStatesStateRegionsImporterTests, NoFilesNoRegions)
 }
 
 
-TEST(Vic3worldStatesStateRegionsImporterTests, SignificantProvincesAreImported)
+TEST(Vic3worldStatesStateRegionsImporterTests, ItemsAreImported)
 {
-   const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/states/SignificantProvincesAreImported", {});
+   const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/states/ItemsAreImported", {});
    const std::map<std::string, StateRegion> state_regions = ImportStateRegions(mod_filesystem);
 
    EXPECT_THAT(state_regions,
        testing::UnorderedElementsAre(testing::Pair("STATE_LOMBARDY",
                                          StateRegion(std::map<std::string, std::string>{{"xD04060", "city"},
-                                             {"x867A90", "farm"},
-                                             {"x3F1E38", "mine"},
-                                             {"x4713EE", "wood"}})),
+                                                         {"x867A90", "farm"},
+                                                         {"x3F1E38", "mine"},
+                                                         {"x4713EE", "wood"}},
+                                             {"x3F1E38", "x4713EE"})),
            testing::Pair("STATE_PIEDMONT",
                StateRegion(std::map<std::string, std::string>{{"xA9397D", "city"},
-                   {"x904061", "port"},
-                   {"x11C061", "farm"},
-                   {"x1140E0", "mine"},
-                   {"xD080E0", "wood"}})),
+                               {"x904061", "port"},
+                               {"x11C061", "farm"},
+                               {"x1140E0", "mine"},
+                               {"xD080E0", "wood"}},
+                   {"x1140E0", "x11C061"})),
            testing::Pair("STATE_SVEALAND",
                StateRegion(std::map<std::string, std::string>{{"x9686A5", "city"},
-                   {"x93C3BC", "port"},
-                   {"xF48646", "farm"},
-                   {"x6F40EC", "mine"},
-                   {"x4C9918", "wood"}}))));
+                               {"x93C3BC", "port"},
+                               {"xF48646", "farm"},
+                               {"x6F40EC", "mine"},
+                               {"x4C9918", "wood"}},
+                   {"x0974E5", "x216569"}))));
 }
 
 }  // namespace vic3
