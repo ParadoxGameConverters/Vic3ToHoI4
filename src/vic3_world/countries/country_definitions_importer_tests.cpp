0f327918f17db631102d9dc84c878095b54e74f2
Idhrendur <idhrendur@hotmail.com>
2023-03-14 17:27:53 -0700

    Import all country definition files.

--- src/vic3_world/countries/country_definitions_importer_tests.cpp
+++ src/vic3_world/countries/country_definitions_importer_tests.cpp
@@ -10,16 +10,9 @@
 namespace vic3
 {
 
-TEST(Vic3WorldCountriesCountriesDefinitionsImporter, ExceptionForMissingFile)
-{
-   commonItems::ModFilesystem mod_filesystem("", {});
-   EXPECT_THROW(const auto tags_to_colors = ImportCountryColorDefinitions(mod_filesystem), std::runtime_error);
-}
-
-
 TEST(Vic3WorldCountriesCountriesDefinitionsImporter, NoDefinitionsByInput)
 {
-   commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/countries/no_definitions_by_default", {});
+   const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/countries/no_definitions_by_default", {});
    const auto tags_to_colors = ImportCountryColorDefinitions(mod_filesystem);
 
    EXPECT_TRUE(tags_to_colors.empty());
@@ -27,12 +20,13 @@ TEST(Vic3WorldCountriesCountriesDefinitionsImporter, NoDefinitionsByInput)
 
 TEST(Vic3WorldCountriesCountriesDefinitionsImporter, DefinitionsCanBeImported)
 {
-   commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/countries/definitions_can_be_imported", {});
+   const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/countries/definitions_can_be_imported", {});
    const auto tags_to_colors = ImportCountryColorDefinitions(mod_filesystem);
 
    EXPECT_THAT(tags_to_colors,
        testing::UnorderedElementsAre(testing::Pair("ONE", commonItems::Color(std::array{1, 1, 1})),
-           testing::Pair("TWO", commonItems::Color(std::array{2, 4, 8}))));
+           testing::Pair("TWO", commonItems::Color(std::array{2, 4, 8})),
+           testing::Pair("THR", commonItems::Color(std::array{3, 9, 27}))));
 }
 
 }  // namespace vic3
