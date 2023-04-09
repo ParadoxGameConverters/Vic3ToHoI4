8aadd77f4884645cc71053c492ec290e182fc5eb
Idhrendur <idhrendur@hotmail.com>
2023-04-08 18:11:55 -0700

    Country mappings are created according to the new signature

--- src/mappers/country/country_mapper_creator_tests.cpp
+++ src/mappers/country/country_mapper_creator_tests.cpp
@@ -2,7 +2,6 @@
 
 #include "external/commonItems/external/googletest/googlemock/include/gmock/gmock-matchers.h"
 #include "external/commonItems/external/googletest/googletest/include/gtest/gtest.h"
-#include "external/fmt/include/fmt/format.h"
 #include "src/mappers/country/country_mapper.h"
 #include "src/mappers/country/country_mapper_creator.h"
 
@@ -13,21 +12,23 @@ namespace mappers
 
 TEST(MappersCountryCountryMapperCreator, GeneratedMappingsCanBeCreated)
 {
-   std::map<int, vic3::Country> source_countries{{1, vic3::Country({.tag = "000"})}};
+   const std::map<int, vic3::Country> source_countries{{1, vic3::Country({.number = 0})}};
 
    const CountryMapper country_mapper = CreateCountryMappings(source_countries);
-   EXPECT_EQ(country_mapper.GetHoiTag("000"), "Z00");
+   EXPECT_EQ(country_mapper.GetHoiTag(0), "Z00");
 }
 
 
 TEST(MappersCountryCountryMapperCreator, GeneratedMappingsIncrementTheNumericDigits)
 {
-   std::map<int, vic3::Country> source_countries{{1, vic3::Country({.tag = "000"})},
-       {2, vic3::Country({.tag = "001"})}};
+   const std::map<int, vic3::Country> source_countries{
+       {1, vic3::Country({.number = 0})},
+       {2, vic3::Country({.number = 1})},
+   };
 
    const CountryMapper country_mapper = CreateCountryMappings(source_countries);
-   EXPECT_EQ(country_mapper.GetHoiTag("000"), "Z00");
-   EXPECT_EQ(country_mapper.GetHoiTag("001"), "Z01");
+   EXPECT_EQ(country_mapper.GetHoiTag(0), "Z00");
+   EXPECT_EQ(country_mapper.GetHoiTag(1), "Z01");
 }
 
 TEST(MappersCountryCountryMapperCreator, GeneratedMappingsDecrementTheAlphaWhenTheNumericsRollOver)
@@ -35,13 +36,13 @@ TEST(MappersCountryCountryMapperCreator, GeneratedMappingsDecrementTheAlphaWhenT
    std::map<int, vic3::Country> source_countries;
    for (int i = 0; i < 102; ++i)
    {
-      source_countries.emplace(i, vic3::Country({.tag = fmt::format("{:0>3}", i)}));
+      source_countries.emplace(i, vic3::Country({.number = i}));
    }
 
    const CountryMapper country_mapper = CreateCountryMappings(source_countries);
-   EXPECT_EQ(country_mapper.GetHoiTag("099"), "Z99");
-   EXPECT_EQ(country_mapper.GetHoiTag("100"), "Y00");
-   EXPECT_EQ(country_mapper.GetHoiTag("101"), "Y01");
+   EXPECT_EQ(country_mapper.GetHoiTag(99), "Z99");
+   EXPECT_EQ(country_mapper.GetHoiTag(100), "Y00");
+   EXPECT_EQ(country_mapper.GetHoiTag(101), "Y01");
 }
 
 }  // namespace mappers
