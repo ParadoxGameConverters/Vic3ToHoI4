fa68101dc05101063caeec4465f428330a2b20e3
Idhrendur <idhrendur@hotmail.com>
2023-04-08 18:43:52 -0700

    Countries converter uses new mappings

--- src/hoi4_world/countries/hoi4_countries_converter_tests.cpp
+++ src/hoi4_world/countries/hoi4_countries_converter_tests.cpp
@@ -16,29 +16,46 @@ namespace hoi4
 
 TEST(Hoi4worldCountriesCountriesConverter, CountriesAreConverted)
 {
-   const mappers::CountryMapper country_mapper({{"TAG", "TAG"}, {"TWO", "TWO"}});
-   const vic3::Country source_country_one(
-       {.tag = "TAG", .color = commonItems::Color{std::array{1, 2, 3}}, .capital_state = 1});
-   const vic3::Country source_country_two(
-       {.tag = "TWO", .color = commonItems::Color{std::array{2, 4, 6}}, .capital_state = 2});
+   const mappers::CountryMapper country_mapper({{1, "TAG"}, {2, "TWO"}});
+   const vic3::Country source_country_one({
+       .number = 1,
+       .color = commonItems::Color{std::array{1, 2, 3}},
+       .capital_state = 1,
+   });
+   const vic3::Country source_country_two({
+       .number = 2,
+       .color = commonItems::Color{std::array{2, 4, 6}},
+       .capital_state = 2,
+   });
 
-   const auto countries = ConvertCountries({{1, source_country_one}, {2, source_country_two}},
+   const auto countries = ConvertCountries(
+       {
+           {1, source_country_one},
+           {2, source_country_two},
+       },
        {
            {1, {"source_technology_one"}},
            {2, {"source_technology_two"}},
        },
        country_mapper,
-       {{1, 10}, {2, 20}},
+       {
+           {1, 10},
+           {2, 20},
+       },
        {},
        {
            {{"source_technology_one"}, std::nullopt, {"dest_technology_one", "dest_technology_two"}},
            {{"source_technology_two"}, std::nullopt, {"dest_technology_three", "dest_technology_four"}},
        });
 
-   const Technologies expected_techs_one{std::map<std::optional<std::string>, std::set<std::string>>{
-       {std::nullopt, std::set<std::string>{"dest_technology_one", "dest_technology_two"}}}};
-   const Technologies expected_techs_two{std::map<std::optional<std::string>, std::set<std::string>>{
-       {std::nullopt, std::set<std::string>{"dest_technology_three", "dest_technology_four"}}}};
+   const Technologies expected_techs_one{
+       std::map<std::optional<std::string>, std::set<std::string>>{
+           {std::nullopt, std::set<std::string>{"dest_technology_one", "dest_technology_two"}}},
+   };
+   const Technologies expected_techs_two{
+       std::map<std::optional<std::string>, std::set<std::string>>{
+           {std::nullopt, std::set<std::string>{"dest_technology_three", "dest_technology_four"}}},
+   };
    const std::vector<EquipmentVariant> expected_legacy_ship_variants_one = {
        EquipmentVariant({"dest_technology_one"}, {}, {{"name", "= \"Test Legacy Ship Variant One\""}}),
        EquipmentVariant({}, {"dest_technology_four"}, {{"name", "= \"Test Legacy Ship Variant Four\""}}),
