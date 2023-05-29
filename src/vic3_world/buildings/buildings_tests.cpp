994df2b09b0208aaf26974b7e5eb746e309b0d0b
Idhrendur <idhrendur@hotmail.com>
2023-05-29 13:54:22 -0700

    Formatting

--- src/vic3_world/buildings/buildings_tests.cpp
+++ src/vic3_world/buildings/buildings_tests.cpp
@@ -90,98 +90,100 @@ TEST(Vic3WorldBuildingsBuildingsTests, TotalGoodsValueInStateAreReported)
 
 TEST(Vic3WorldBuildingsBuildingsTests, NoBuildingsNoIndustryGoodsValue)
 {
-    const Buildings buildings;
+   const Buildings buildings;
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 0.0F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 0.0F);
 }
 
 
 TEST(Vic3WorldBuildingsBuildingsTests, TotalIndustryGoodsValueInWorldAreReported)
 {
-    const Buildings buildings({
-        {1,
-            {
-                Building("building_tooling_workshops", std::nullopt, 1.0F),
-            }},
-        {2,
-            {
-                Building("building_tooling_workshops", std::nullopt, 0.5F),
-                Building("building_tooling_workshops", std::nullopt, 0.25F),
-            }},
-        });
+   const Buildings buildings({
+       {1,
+           {
+               Building("building_tooling_workshops", std::nullopt, 1.0F),
+           }},
+       {2,
+           {
+               Building("building_tooling_workshops", std::nullopt, 0.5F),
+               Building("building_tooling_workshops", std::nullopt, 0.25F),
+           }},
+   });
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 1.75F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 1.75F);
 }
 
 
 TEST(Vic3WorldBuildingsBuildingsTests, NoIndustryGoodsValueForNonIndustrialBuilding)
 {
-    const Buildings buildings({
-        {1,
-            {
-                Building("building_tooling_workshops", std::nullopt, 1.0F),
-            }},
-        {2,
-            {
-                Building("a_farm", std::nullopt, 0.5F),
-                Building("building_tooling_workshops", std::nullopt, 0.25F),
-            }},
-        });
+   const Buildings buildings({
+       {1,
+           {
+               Building("building_tooling_workshops", std::nullopt, 1.0F),
+           }},
+       {2,
+           {
+               Building("a_farm", std::nullopt, 0.5F),
+               Building("building_tooling_workshops", std::nullopt, 0.25F),
+           }},
+   });
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 1.25F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInWorld(), 1.25F);
 }
 
 
 TEST(Vic3WorldBuildingsBuildingsTests, NoIndustryGoodsValueInMissingState)
 {
-    const Buildings buildings({
-        {1,
-            {
-                Building("building_tooling_workshops", std::nullopt, 1.0F),
-            }},
-        {2,
-            {
-                Building("building_tooling_workshops", std::nullopt, 0.5F),
-                Building("building_tooling_workshops", std::nullopt, 0.25F),
-            }},
-        });
+   const Buildings buildings({
+       {1,
+           {
+               Building("building_tooling_workshops", std::nullopt, 1.0F),
+           }},
+       {2,
+           {
+               Building("building_tooling_workshops", std::nullopt, 0.5F),
+               Building("building_tooling_workshops", std::nullopt, 0.25F),
+           }},
+   });
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(3), 0.0F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(3), 0.0F);
 }
 
 
 TEST(Vic3WorldBuildingsBuildingsTests, NoIndustryGoodsValueInStateWithNoBuildings)
 {
-    const Buildings buildings({ {1,
-                                   {
-                                       Building("", std::nullopt, 1.0F),
-                                   }},
-        {2,
-            {
-                Building("building_tooling_workshops", std::nullopt, 0.5F),
-                Building("building_tooling_workshops", std::nullopt, 0.25F),
-            }},
-        {3, {}} });
+   const Buildings buildings({
+       {1,
+           {
+               Building("building_tooling_workshops", std::nullopt, 1.0F),
+           }},
+       {2,
+           {
+               Building("building_tooling_workshops", std::nullopt, 0.5F),
+               Building("building_tooling_workshops", std::nullopt, 0.25F),
+           }},
+       {3, {}},
+   });
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(3), 0.0F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(3), 0.0F);
 }
 
 
 TEST(Vic3WorldBuildingsBuildingsTests, TotalIndustryGoodsValueInStateAreReported)
 {
-    const Buildings buildings({
-        {1,
-            {
-                Building("building_tooling_workshops", std::nullopt, 1.0F),
-            }},
-        {2,
-            {
-                Building("building_tooling_workshops", std::nullopt, 0.5F),
-                Building("building_tooling_workshops", std::nullopt, 0.25F),
-            }},
-        });
+   const Buildings buildings({
+       {1,
+           {
+               Building("building_tooling_workshops", std::nullopt, 1.0F),
+           }},
+       {2,
+           {
+               Building("building_tooling_workshops", std::nullopt, 0.5F),
+               Building("building_tooling_workshops", std::nullopt, 0.25F),
+           }},
+   });
 
-    EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(2), 0.75F);
+   EXPECT_EQ(buildings.GetTotalIndustryGoodSalesValueInState(2), 0.75F);
 }
 
 
