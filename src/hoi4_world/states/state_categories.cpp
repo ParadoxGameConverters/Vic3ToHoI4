b4c32613d48ef515af8a787160d66621f4c38c58
Idhrendur <idhrendur@hotmail.com>
2023-05-30 18:42:47 -0700

    Log stats about state categories

--- src/hoi4_world/states/state_categories.cpp
+++ src/hoi4_world/states/state_categories.cpp
@@ -18,3 +18,17 @@ std::string hoi4::StateCategories::GetBestCategory(int num_building_slots) const
 
    return category;
 }
+
+
+int hoi4::StateCategories::GetNumSlots(const std::string& category_name_to_match) const
+{
+   for (auto [category_slots, category_name]: categories_)
+   {
+      if (category_name_to_match == category_name)
+      {
+         return category_slots;
+      }
+   }
+
+   return 0;
+}
