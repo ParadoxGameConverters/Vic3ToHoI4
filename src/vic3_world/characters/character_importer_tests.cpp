dda90c1de5d49055f649298d4b554c0d7723e965
Gawquon <gawquonthestoryteller@gmail.com>
2023-06-20 23:42:31 -0400

    clang

--- src/vic3_world/characters/character_importer_tests.cpp
+++ src/vic3_world/characters/character_importer_tests.cpp
@@ -83,7 +83,6 @@ TEST(Vic3WorldCharactersCharacterImporter, InvalidRankIsLogged)
    std::cout.rdbuf(cout_buffer);
 
    EXPECT_EQ(character, Character({.id = 1, .rank = 0}));
-   EXPECT_THAT(log.str(),
-       testing::HasSubstr("[WARNING] Failed to read rank: commander_rank_modded."));
+   EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Failed to read rank: commander_rank_modded."));
 }
 }  // namespace vic3
