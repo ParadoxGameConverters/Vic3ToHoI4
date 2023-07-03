b81bfcef974dcbc9a4e1785638be0dc9957b9a1a
Gawquon <gawquonthestoryteller@gmail.com>
2023-07-03 12:38:10 -0400

    Leader type mappers, for if king or prime minister is true ruler

--- src/mappers/character/culture_queue.h
+++ src/mappers/character/culture_queue.h
@@ -5,28 +5,30 @@
 
 namespace mappers
 {
+using CharacterId = int;
+
 struct IdeologyCultureQueue
 {
-   std::vector<int> communism;
-   std::vector<int> democratic;
-   std::vector<int> fascism;
-   std::vector<int> neutrality;
+   std::vector<CharacterId> communism;
+   std::vector<CharacterId> democratic;
+   std::vector<CharacterId> fascism;
+   std::vector<CharacterId> neutrality;
 
    std::partial_ordering operator<=>(const IdeologyCultureQueue&) const = default;
 };
 
 struct CultureQueue  // Mirrors PortraitPaths of GraphicsBlock
 {
-   std::vector<int> army;
-   std::vector<int> navy;
+   std::vector<CharacterId> army;
+   std::vector<CharacterId> navy;
    IdeologyCultureQueue leader;
-   std::vector<int> female_leader;
+   std::vector<CharacterId> female_leader;
    IdeologyCultureQueue advisor;
-   std::vector<int> male_operative;
-   std::vector<int> female_operative;
-   std::vector<int> male_monarch;
-   std::vector<int> female_monarch;
-   std::vector<int> council;
+   std::vector<CharacterId> male_operative;
+   std::vector<CharacterId> female_operative;
+   std::vector<CharacterId> male_monarch;
+   std::vector<CharacterId> female_monarch;
+   std::vector<CharacterId> council;
 
    std::partial_ordering operator<=>(const CultureQueue&) const = default;
 };
---
+++ src/mappers/character/leader_type_mapper.cpp
@@ -0,0 +1,23 @@
+﻿#include "src/mappers/character/leader_type_mapper.h"
+
+#include <ranges>
+
+mappers::LeaderType mappers::LeaderTypeMapper::GetCountryLeaderType(const std::set<std::string>& current_laws) const
+{
+   // If no mapping is found, head of state is assumed
+   for (const std::string& governance_law: rules_ | std::views::keys)
+   {
+      if (current_laws.contains(governance_law))
+      {
+         for (const auto& [power_law, leader_type]: rules_.at(governance_law))
+         {
+            if (current_laws.contains(power_law))
+            {
+               return leader_type;
+            }
+         }
+         return kHeadOfState;
+      }
+   }
+   return kHeadOfState;
+}
