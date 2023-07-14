7c61f2b5c7ef867765b96c256e4e7495a5b53d57
Stephen May <idhrendur@hotmail.com>
2023-07-13 19:54:26 -0700

    Clang format

--- src/vic3_world/states/state_regions_importer_tests.cpp
+++ src/vic3_world/states/state_regions_importer_tests.cpp
@@ -49,20 +49,31 @@ TEST(Vic3worldStatesStateRegionsImporterTests, ItemsAreImported)
 
 TEST(Vic3worldStatesStateRegionsImporterTests, BadSpecialProvincesAreLogged)
 {
-    std::stringstream log;
-    std::streambuf* cout_buffer = std::cout.rdbuf();
-    std::cout.rdbuf(log.rdbuf());
+   std::stringstream log;
+   std::streambuf* cout_buffer = std::cout.rdbuf();
+   std::cout.rdbuf(log.rdbuf());
 
-    const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/states/BadSpecialProvincesAreLogged/game", {});
-    const std::map<std::string, StateRegion> _ = ImportStateRegions(mod_filesystem);
+   const commonItems::ModFilesystem mod_filesystem("test_files/vic3_world/states/BadSpecialProvincesAreLogged/game",
+       {});
+   const std::map<std::string, StateRegion> _ = ImportStateRegions(mod_filesystem);
 
-    std::cout.rdbuf(cout_buffer);
+   std::cout.rdbuf(cout_buffer);
 
-    EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Significant province x9686A5 (city) does not correspond to a province in state region STATE_SVEALAND."));
-    EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Significant province x93C3BC (port) does not correspond to a province in state region STATE_SVEALAND."));
-    EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Significant province xF48646 (farm) does not correspond to a province in state region STATE_SVEALAND."));
-    EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Significant province x6F40EC (mine) does not correspond to a province in state region STATE_SVEALAND."));
-    EXPECT_THAT(log.str(), testing::HasSubstr("[WARNING] Significant province x4C9918 (wood) does not correspond to a province in state region STATE_SVEALAND."));
+   EXPECT_THAT(log.str(),
+       testing::HasSubstr("[WARNING] Significant province x9686A5 (city) does not correspond to a province in state "
+                          "region STATE_SVEALAND."));
+   EXPECT_THAT(log.str(),
+       testing::HasSubstr("[WARNING] Significant province x93C3BC (port) does not correspond to a province in state "
+                          "region STATE_SVEALAND."));
+   EXPECT_THAT(log.str(),
+       testing::HasSubstr("[WARNING] Significant province xF48646 (farm) does not correspond to a province in state "
+                          "region STATE_SVEALAND."));
+   EXPECT_THAT(log.str(),
+       testing::HasSubstr("[WARNING] Significant province x6F40EC (mine) does not correspond to a province in state "
+                          "region STATE_SVEALAND."));
+   EXPECT_THAT(log.str(),
+       testing::HasSubstr("[WARNING] Significant province x4C9918 (wood) does not correspond to a province in state "
+                          "region STATE_SVEALAND."));
 }
 
 }  // namespace vic3
