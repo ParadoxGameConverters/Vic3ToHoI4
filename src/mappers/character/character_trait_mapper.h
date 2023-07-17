9572adade9b08bc879e4c7d992239132974f832a
Gawquon <gawquonthestoryteller@gmail.com>
2023-07-17 02:50:30 -0400

    prep character_trait_mapper

---
+++ src/mappers/character/character_trait_mapper.h
@@ -0,0 +1,68 @@
+﻿#ifndef SRC_MAPPERS_CHARACTER_CHARACTERTRAITMAPPER_H
+#define SRC_MAPPERS_CHARACTER_CHARACTERTRAITMAPPER_H
+
+
+
+#include <map>
+#include <string>
+
+
+namespace hoi4
+{
+using Trait = std::string;
+}
+namespace vic3
+{
+using Trait = std::string;
+}
+
+namespace mappers
+{
+
+struct GeneralTraitMapping
+{
+   std::set<hoi4::Trait> traits;
+   attack = 0;
+   defense = 0;
+   planning = 0;
+   logistics = 0;
+};
+struct AdmiralTraitMapping
+{
+   std::set<hoi4::Trait> traits;
+   attack = 0;
+   defense = 0;
+   maneuvering = 0;
+   coordination = 0;
+};
+using GeneralTraitMap = std::map<vic3::Trait, GeneralTraitMapping>;
+using AdmiralTraitMap = std::map<vic3::Trait, AdmiralTraitMapping>;
+using SpyTraitMap = std::map<vic3::Trait, hoi4::Trait>;
+
+class CharacterTraitMapper
+{
+  public:
+   explicit CharacterTraitMapper(GeneralTraitMap general_trait_rules,
+       AdmiralTraitMap admiral_trait_rules,
+       SpyTraitMap spy_trait_rules):
+       general_trait_rules_(std::move(general_trait_rules)),
+       admiral_trait_rules_(std::move(admiral_trait_rules)),
+       spy_trait_rules_(std::move(spy_trait_rules))
+   {
+   }
+
+   [[nodiscard]] GeneralTraitMapping GetGeneralMappedData(const std::set<vic3::Trait>& source_traits) const;
+   [[nodiscard]] AdmiralTraitMapping GetAdmiralMappedData(const std::set<vic3::Trait>& source_traits) const;
+   [[nodiscard]] std::set<hoi4::Trait> GetSpyMappedTraits(const std::set<vic3::Trait>& source_traits) const;
+
+  private:
+   GeneralTraitMap general_trait_rules_;
+   AdmiralTraitMap admiral_trait_rules_;
+   SpyTraitMap spy_trait_rules_;
+};
+
+}  // namespace mappers
+
+
+
+#endif  // SRC_MAPPERS_CHARACTER_LEADERTYPEMAPPER_H
---
+++ src/mappers/character/character_trait_mapper_importer_tests.cpp
@@ -0,0 +1,23 @@
+﻿#include <sstream>
+
+#include "external/commonItems/external/googletest/googlemock/include/gmock/gmock-matchers.h"
+#include "external/commonItems/external/googletest/googletest/include/gtest/gtest.h"
+#include "src/mappers/character/character_trait_mapper_importer.h"
+
+
+
+namespace mappers
+{
+
+TEST(MappersCharacterCharactertraitmapperimporter, DISABLED_TBD)
+{
+   const CharacterTraitMapper character_trait_mapper =
+       ImportCharacterTraitMapper("test_files/configurables/character_traits.txt");
+
+   //    EXPECT_EQ(character_trait_mapper.GetGeneralTraitRules() , TBD);
+   //    EXPECT_EQ(character_trait_mapper.GetAdmiralTraitRules() , TBD);
+   //    EXPECT_EQ(character_trait_mapper.GetSpyTraitRules() , TBD);
+}
+
+
+}  // namespace mappers
