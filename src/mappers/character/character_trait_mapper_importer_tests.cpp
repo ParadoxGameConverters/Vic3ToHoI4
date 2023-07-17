20d405684cc3cfecb610e484babfe2cd38fa6d08
Gawquon <gawquonthestoryteller@gmail.com>
2023-07-17 05:20:09 -0400

    eq/that mixup

--- src/mappers/character/character_trait_mapper_importer_tests.cpp
+++ src/mappers/character/character_trait_mapper_importer_tests.cpp
@@ -19,8 +19,8 @@ TEST(MappersCharacterCharactertraitmapperimporter, DISABLED_TBD)
        AdmiralTraitMapping({"naval_trait_0", "naval_trait_1"}, 2, 2, 2, 2));
    EXPECT_EQ(character_trait_mapper.GetGeneralMappedData({"charismatic"}, true),
        GeneralTraitMapping({"land_trait_0", "land_trait_1"}, {"charismatic"}, 2, 2, 2, 2));
-   EXPECT_EQ(character_trait_mapper.GetSpyMappedTraits({"bandit"}), testing::UnorderedElementsAre("operative_tough"));
-   EXPECT_EQ(character_trait_mapper.GetSpyMappedTraits({"persistent"}),
+   EXPECT_THAT(character_trait_mapper.GetSpyMappedTraits({"bandit"}), testing::UnorderedElementsAre("operative_tough"));
+   EXPECT_THAT(character_trait_mapper.GetSpyMappedTraits({"persistent"}),
        testing::UnorderedElementsAre("operative_tough"));
 }
 
