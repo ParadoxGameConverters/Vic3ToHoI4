4d7215a0df152339433a981299e466dc722edbdf
Michael Ebert <mebert@pacbell.net>
2023-08-01 18:02:40 -0700

    removed pch

--- src/mappers/world/world_mapper.cpp
+++ src/mappers/world/world_mapper.cpp
@@ -1,12 +1,10 @@
 #include "world_mapper.h"
 
-#include <src/mappers/culture/culture_graphics_mapper.h>
-#include <src/mappers/culture/culture_graphics_mapper_importer.h>
-#include <src/mappers/technology/tech_mapping.h>
-#include <src/mappers/technology/tech_mappings_importer.h>
-
-#include "pch.h"
 #include "src/mappers/country/country_mapper_creator.h"
+#include "src/mappers/culture/culture_graphics_mapper.h"
+#include "src/mappers/culture/culture_graphics_mapper_importer.h"
+#include "src/mappers/technology/tech_mapping.h"
+#include "src/mappers/technology/tech_mappings_importer.h"
 
 namespace mappers
 {
