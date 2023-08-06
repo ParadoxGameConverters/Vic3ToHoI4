eafe89b9dba7d4c84b183be2306ca7903b65df7d
Michael Ebert <mebert@pacbell.net>
2023-08-05 17:48:50 -0700

    added new files

---
+++ src/hoi4_world/world/hoi4_world_framework.cpp
@@ -0,0 +1,72 @@
+#include <src/hoi4_world/states/state_categories.h>
+#include <src/hoi4_world/map/resources_map_importer.h>
+#include <src/hoi4_world/states/default_states_importer.h>
+#include <src/hoi4_world/map/strategic_regions_importer.h>
+#include "src/hoi4_world/world/hoi4_world_framework.h"
+
+namespace hoi4
+{
+WorldFrameworkBuilder WorldFrameworkBuilder::CreateDefaultWorldFramework(
+    const commonItems::ModFilesystem& hoi4_mod_filesystem)
+{
+   WorldFrameworkBuilder wfb{};
+   wfb.hoi4_mod_filesystem = hoi4_mod_filesystem;
+   wfb.DefaultStrategicRegions();
+   wfb.DefaultDefaultStates();
+   wfb.DefaultResourcesMap();
+   wfb.DefaultStateCategories();
+   return wfb;
+}
+
+WorldFrameworkBuilder WorldFrameworkBuilder::CreateNullWorldFramework()
+{
+   WorldFrameworkBuilder wfb{};
+   wfb.hoi4_mod_filesystem = commonItems::ModFilesystem("test_files/hoi4_world", {});
+   wfb.DefaultStrategicRegions();
+   wfb.DefaultDefaultStates();
+   wfb.DefaultResourcesMap();
+   wfb.DefaultStateCategories();
+   return wfb;
+}
+WorldFramework WorldFrameworkBuilder::Build()
+{
+   return WorldFramework(std::move(this->strategic_regions),
+       std::move(this->default_states),
+       std::move(this->resources_map),
+       std::move(this->state_categories)
+       );
+}
+
+WorldFrameworkBuilder& WorldFrameworkBuilder::DefaultStrategicRegions()
+{
+   this->strategic_regions = ImportStrategicRegions(this->hoi4_mod_filesystem);
+   return *this;
+}
+
+WorldFrameworkBuilder& WorldFrameworkBuilder::DefaultDefaultStates()
+{
+   this->default_states = hoi4::ImportDefaultStates(this->hoi4_mod_filesystem);
+   return *this;
+}
+WorldFrameworkBuilder& WorldFrameworkBuilder::DefaultResourcesMap()
+{
+   this->resources_map = ImportResources("configurables/resources.txt");
+   return *this;
+}
+WorldFrameworkBuilder& WorldFrameworkBuilder::DefaultStateCategories()
+{
+   this->state_categories = StateCategories({
+       {1, "pastoral"},
+       {2, "rural"},
+       {4, "town"},
+       {5, "large_town"},
+       {6, "city"},
+       {8, "large_city"},
+       {10, "metropolis"},
+       {12, "megalopolis"},
+   });
+   return *this;
+}
+//WorldFrameworkBuilder& WorldFrameworkBuilder::DefaultXXX()
+
+}
---
+++ src/hoi4_world/world/hoi4_world_framework.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <src/hoi4_world/states/state_categories.h>
+#include <src/hoi4_world/states/default_state.h>
+#include <src/hoi4_world/map/strategic_regions.h>
+
+// This represents the input hoi4 elements (eg, state maps, resource maps, etc)
+// that are used in conjunction with the vic3 data  and the WorldMapper to create the final hoi4 mod.
+
+namespace hoi4
+{
+class WorldFramework
+{
+  public:
+   WorldFramework(StrategicRegions&& strategic_regions,
+       std::map<int, DefaultState>&& default_states,
+       ResourcesMap&& resources_map,
+       StateCategories&& state_categories):
+       strategic_regions(std::move(strategic_regions)),
+       default_states(std::move(default_states)),
+       resources_map(std::move(resources_map)),
+       state_categories(std::move(state_categories))
+   {
+   }
+
+   StrategicRegions strategic_regions;
+   const std::map<int, DefaultState> default_states;
+   const ResourcesMap resources_map;
+   const StateCategories state_categories;
+};
+
+
+class WorldFrameworkBuilder
+{
+  public:
+   static WorldFrameworkBuilder CreateDefaultWorldFramework(const commonItems::ModFilesystem& hoi4_mod_filesystem);
+   static WorldFrameworkBuilder CreateNullWorldFramework();
+   WorldFramework Build();
+
+   WorldFrameworkBuilder& DefaultStrategicRegions();
+   WorldFrameworkBuilder& DefaultDefaultStates();
+   WorldFrameworkBuilder& DefaultResourcesMap();
+   WorldFrameworkBuilder& DefaultStateCategories();
+
+  private:
+   WorldFrameworkBuilder();
+   commonItems::ModFilesystem& hoi4_mod_filesystem;
+   StrategicRegions strategic_regions;
+   std::map<int, DefaultState> default_states;
+   ResourcesMap resources_map;
+   StateCategories state_categories;
+};
+}  // namespace hoi4
