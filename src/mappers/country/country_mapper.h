af4dc1555760925d7aa02d5b4e4e0e5ff9f40dd8
Michael Ebert <mebert@pacbell.net>
2023-08-06 13:30:06 -0700

    removed explicit move constructor

--- src/mappers/country/country_mapper.h
+++ src/mappers/country/country_mapper.h
@@ -16,11 +16,9 @@ class CountryMapper
 {
   public:
    CountryMapper() = default;
-   explicit CountryMapper(std::map<int, std::string>&& country_mappings): country_mappings_(std::move(country_mappings))
+   explicit CountryMapper(std::map<int, std::string> country_mappings): country_mappings_(std::move(country_mappings))
    {
    }
-   explicit CountryMapper(const std::map<int, std::string>& country_mappings): country_mappings_(country_mappings) {}
-
    [[nodiscard]] std::optional<std::string> GetHoiTag(const int& vic_number) const;
 
    [[nodiscard]] const std::map<int, std::string>& GetCountryMappings() const { return country_mappings_; }
