fa319b7de273841d95a2e0cbe1def725a2dda64a
Michael Ebert <mebert@pacbell.net>
2023-08-22 00:17:21 -0700

    aa

--- src/vic3_world/institutions/institution.h
+++ src/vic3_world/institutions/institution.h
@@ -8,6 +8,5 @@ struct Institution
    int country = 0;
    int investment = 0;
    auto operator<=>(Institution const& other) const = default;
-   //bool operator==(Institution const& other) const = default;
 };
 }  // namespace vic3
