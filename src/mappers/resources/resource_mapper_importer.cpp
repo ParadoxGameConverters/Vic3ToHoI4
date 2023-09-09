04b92ca6a821b1e636b361cc4c539f13e5295087
kingofmen <rolf.andreassen@gmail.com>
2023-09-08 22:51:26 -0700

    Add ResourceMapperImporter and test.

---
+++ src/mappers/resources/resource_mapper_importer.cpp
@@ -0,0 +1,42 @@
+#include "src/mappers/resources/resource_mapper_importer.h"
+
+#include "external/commonItems/CommonRegexes.h"
+#include "external/commonItems/Parser.h"
+#include "external/commonItems/ParserHelpers.h"
+
+mappers::ResourceMapper mappers::ImportResourceMapper(std::string_view mapping_file)
+{
+   std::map<std::string, mappers::ResourceScore> scores;
+   mappers::ResourceScore current;
+
+
+   commonItems::parser base_parser;
+   base_parser.registerRegex(commonItems::catchallRegex,
+       [&current](const std::string& key, std::istream& input_stream) {
+          current.base.emplace(key, commonItems::getDouble(input_stream));
+       });
+
+   commonItems::parser score_parser;
+   score_parser.registerKeyword("base", [&base_parser](const std::string& key, std::istream& input_stream) {
+      base_parser.parseStream(input_stream);
+   });
+   score_parser.registerKeyword("bonus", [&current](std::istream& input_stream) {
+      current.bonus = commonItems::getDouble(input_stream);
+   });
+   score_parser.registerKeyword("bonus_building", [&current](std::istream& input_stream) {
+      current.bonus_buildings.emplace(commonItems::getString(input_stream));
+   });
+
+   commonItems::parser file_parser;
+   file_parser.registerRegex(commonItems::catchallRegex,
+       [&score_parser, &scores, &current](const std::string& resource, std::istream& input_stream) {
+          current.base.clear();
+          current.bonus_buildings.clear();
+          current.bonus = 0;
+          score_parser.parseStream(input_stream);
+          scores.emplace(resource, current);
+       });
+
+   file_parser.parseFile(mapping_file);
+   return mappers::ResourceMapper(scores);
+}
---
+++ src/mappers/resources/resource_mapper_importer_tests.cpp
@@ -0,0 +1,43 @@
+#include <sstream>
+
+#include "external/commonItems/external/googletest/googlemock/include/gmock/gmock-matchers.h"
+#include "external/commonItems/external/googletest/googletest/include/gtest/gtest.h"
+#include "src/mappers/resources/resource_mapper.h"
+#include "src/mappers/resources/resource_mapper_importer.h"
+
+constexpr float kTolerance = 0.0001F;
+
+namespace mappers
+{
+
+TEST(MappersResourcesResourceMapperImporterTests, RulesCanBeImported)
+{
+   const ResourceMapper mapper = ImportResourceMapper("test_files/configurables/resource_mappings.txt");
+   std::vector<vic3::Building> buildings{
+     {"building_iron_mine",
+      1,
+      1.0F,
+      1.0F,
+      {}},
+     {"building_oil_rig",
+      1,
+      0.8F,
+      1.0F,
+      {}},
+     {"building_coal_mine",
+      1,
+      0.2F,
+      1.0F,
+      {}},
+     {"building_steel_factory",
+      1,
+      0.8F,
+      1.0F,
+      {}},
+   };
+   EXPECT_NEAR(mapper.CalculateScore("steel", buildings), 1.1F, kTolerance);
+   EXPECT_NEAR(mapper.CalculateScore("oil", buildings), 0.48F, kTolerance);
+   EXPECT_NEAR(mapper.CalculateScore("tungsten", buildings), 0.0F, kTolerance);
+}
+
+}  // namespace mappers
