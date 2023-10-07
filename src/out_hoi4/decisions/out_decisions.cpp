97f99fe4e7f07466985b0dbd12bc0b7a24a22eef
kingofmen <rolf.andreassen@gmail.com>
2023-10-07 12:04:21 -0700

    Stub code for outputting generated decisions.

---
+++ src/out_hoi4/decisions/out_decisions.cpp
@@ -0,0 +1,14 @@
+#include "src/out_hoi4/decisions/out_decisions.h"
+
+#include "external/commonItems/Log.h"
+
+
+namespace out
+{
+
+void OutputDecisions(std::string_view output_name, const std::map<std::string, hoi4::Country>& countries)
+{
+   Log(LogLevel::Debug) << "Decisions not implemented yet, no decisions output.";
+}
+
+}  // namespace out
