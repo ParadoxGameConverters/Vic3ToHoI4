be532e214c8e100f3cd332b2e74b60b0f00c7af3
kingofmen <rolf.andreassen@gmail.com>
2023-10-26 23:45:31 -0700

    Ship fields.

--- src/hoi4_world/military/ship.h
+++ src/hoi4_world/military/ship.h
@@ -9,10 +9,29 @@ namespace hoi4
 class Ship
 {
   public:
-   Ship() {}
+   Ship(const std::string& name,
+       const std::string& definition,
+       const std::string& equipment,
+       const std::string& version):
+       name_(name),
+       definition_(definition),
+       equipment_(equipment),
+       version_(version)
+   {
+   }
+
+   const std::string& GetName() const { return name_; }
+   const std::string& GetDefinition() const { return definition_; }
+   const std::string& GetEquipment() const { return equipment_; }
+   const std::string& GetVersion() const { return version_; }
 
    std::partial_ordering operator<=>(const Ship&) const = default;
+
   private:
+   std::string name_;
+   std::string definition_;
+   std::string equipment_;
+   std::string version_;
 };
 
 }  // namespace hoi4
