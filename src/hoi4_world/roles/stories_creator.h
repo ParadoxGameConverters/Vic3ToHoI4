6cf78a983210b4643af5f5579d0d7ee3670182c3
Idhrendur <idhrendur@hotmail.com>
2024-04-07 17:57:09 -0700

    Move the work on creating stories to its own file.

---
+++ src/hoi4_world/roles/stories_creator.h
@@ -0,0 +1,15 @@
+#ifndef SRC_HOI4WORLD_ROLES_STORIESCREATOR_H
+#define SRC_HOI4WORLD_ROLES_STORIESCREATOR_H
+
+
+
+namespace hoi4
+{
+
+void CreateStories();
+
+}  // namespace hoi4
+
+
+
+#endif  // SRC_HOI4WORLD_ROLES_STORIESCREATOR_H
