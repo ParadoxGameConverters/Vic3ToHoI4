410573874536bb43d89a18fc60b60cc34e81025e
Idhrendur <idhrendur@hotmail.com>
2024-05-12 23:23:19 -0700

    Add room for tests.

---
+++ src/hoi4_world/roles/stories_creator_tests.cpp
@@ -0,0 +1,17 @@
+#include <sstream>
+
+#include "external/commonItems/external/googletest/googlemock/include/gmock/gmock-matchers.h"
+#include "external/commonItems/external/googletest/googletest/include/gtest/gtest.h"
+#include "src/hoi4_world/roles/stories_creator.h"
+
+
+
+namespace hoi4
+{
+
+TEST(Hoi4worldRolesStoriescreatorTests, Foo)
+{
+   EXPECT_TRUE(false);
+}
+
+}  // namespace hoi4
