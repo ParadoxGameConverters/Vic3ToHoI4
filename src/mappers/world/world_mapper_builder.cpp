c4b41005652f98c3c40c4d6a07b9f9453a0f393a
Cocopioche <95039203+Cocopioche@users.noreply.github.com>
2024-07-12 14:55:57 -0400

    send world data in constructor

--- src/mappers/world/world_mapper_builder.cpp
+++ src/mappers/world/world_mapper_builder.cpp
@@ -18,7 +18,9 @@ namespace mappers
 WorldMapperBuilder WorldMapperBuilder::CreateDefaultMapper(commonItems::ModFilesystem hoi4_mod_filesystem,
     const vic3::World& source_world)
 {
-   const auto province_mapper = mappers::ProvinceMapperImporter(hoi4_mod_filesystem).ImportProvinceMappings();
+   Log(LogLevel::Info) << "Starting Province Maping";
+   auto province_mapper = mappers::ProvinceMapperImporter(hoi4_mod_filesystem, source_world).ImportProvinceMappings();
+
 
    WorldMapperBuilder builder = WorldMapperBuilder();
    builder.DefaultCountryMapper(source_world);
