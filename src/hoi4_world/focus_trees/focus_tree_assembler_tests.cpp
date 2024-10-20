54ff3499818907722b75a547311a54d127dc8b27
Idhrendur <idhrendur@hotmail.com>
2024-10-20 14:04:05 -0700

    One more test.

--- src/hoi4_world/focus_trees/focus_tree_assembler_tests.cpp
+++ src/hoi4_world/focus_trees/focus_tree_assembler_tests.cpp
@@ -294,4 +294,75 @@ TEST(Hoi4worldFocustreesFocustreeassemblerTests, PrerequisitesWithRepeatFocusesA
            }));
 }
 
+
+// The previous test impicility covers even numbers of repeat focuses, this one does odd
+TEST(Hoi4worldFocustreesFocustreeassemblerTests, FocusesAfterRepeatFocusesAreBalanced)
+{
+   const FocusTree focus_tree = AssembleTree(
+       {
+           Role{{
+               .focuses =
+                   {
+                       Focus{.id = "$TAG$_focus_one", .tree_starter = true},
+                       Focus{.id = "$TAG$_focus_three", .prerequisites = {"repeat_focus = $TARGET_TAG$_focus_two"}},
+                   },
+               .repeat_focuses =
+                   {
+                       RepeatFocus{
+                           .requirement =
+                               [](const Country&, const World&) {
+                                  return true;
+                               },
+                           .focuses =
+                               {
+                                   Focus{
+                                       .id = "$TARGET_TAG$_focus_two",
+                                       .prerequisites = {"$TAG$_focus_one"},
+                                       .relative_position_id = "$TAG$_focus_one",
+                                   },
+                               },
+                       },
+                   },
+           }},
+       },
+       "TAG",
+       World({.countries = {
+                  {"ONE", Country({})},
+                  {"TWO", Country({})},
+                  {"THR", Country({})},
+              }}));
+
+   EXPECT_TRUE(focus_tree.shared_focuses.empty());
+   EXPECT_THAT(focus_tree.focuses,
+       testing::ElementsAre(
+           Focus{
+               .id = "TAG_focus_one",
+               .tree_starter = true,
+           },
+           Focus{
+               .id = "TAG_focus_three",
+               .prerequisites = {"focus = ONE_focus_two focus = THR_focus_two focus = TWO_focus_two"},
+               .x_position = 0,
+               .relative_position_id = "THR_focus_two",
+           },
+           Focus{
+               .id = "ONE_focus_two",
+               .prerequisites = {"TAG_focus_one"},
+               .x_position = -2,
+               .relative_position_id = "TAG_focus_one",
+           },
+           Focus{
+               .id = "THR_focus_two",
+               .prerequisites = {"TAG_focus_one"},
+               .x_position = 0,
+               .relative_position_id = "TAG_focus_one",
+           },
+           Focus{
+               .id = "TWO_focus_two",
+               .prerequisites = {"TAG_focus_one"},
+               .x_position = 2,
+               .relative_position_id = "TAG_focus_one",
+           }));
+}
+
 }  // namespace hoi4
