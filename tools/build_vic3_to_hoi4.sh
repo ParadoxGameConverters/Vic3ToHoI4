bd05fa321eab0af0cf179a491833c002140d5a70
Idhrendur <idhrendur@hotmail.com>
2024-11-03 13:29:13 -0800

    Codacy fix

--- tools/build_vic3_to_hoi4.sh
+++ tools/build_vic3_to_hoi4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cmake --preset x64-release-linux
 cmake --build --preset build-x64-release-linux --target Vic3ToHoi4 ConverterFrontend Vic3ToHoi4Tests -- -j40
-cd build/test/Release-Linux
+cd build/test/Release-Linux || exit
 ./Vic3ToHoi4Tests
 cd ..
