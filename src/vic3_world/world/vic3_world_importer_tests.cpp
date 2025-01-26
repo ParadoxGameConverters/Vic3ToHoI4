ff41be6711bb3856fa6661cf2ef37ec60481b2e4
Idhrendur <idhrendur@hotmail.com>
2025-01-25 23:12:08 -0800

    Formatting

--- src/vic3_world/world/vic3_world_importer_tests.cpp
+++ src/vic3_world/world/vic3_world_importer_tests.cpp
@@ -287,7 +287,7 @@ TEST(Vic3worldWorldVic3worldimporter, ModsInSaveAreLogged)
            "[INFO] \t\t->> Found potentially useful [Test Mod]: test_files/vic3_world/documents/mod/test_mod"));
    EXPECT_THAT(log.str(),
        testing::HasSubstr("[INFO] \t\t->> Found potentially useful [Test Mod Two]: "
-           "test_files/vic3_world/workshop/529340/test_mod_two"));
+                          "test_files/vic3_world/workshop/529340/test_mod_two"));
 #endif
 }
 
