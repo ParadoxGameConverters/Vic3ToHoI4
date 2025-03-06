094841d81201301aa2e68bed451c89f17400874a
Idhrendur <idhrendur@hotmail.com>
2025-03-06 08:52:23 -0800

    Output decisions.

--- src/out_hoi4/decisions/out_decisions_tests.cpp
+++ src/out_hoi4/decisions/out_decisions_tests.cpp
@@ -1,24 +1,80 @@
+#include <external/commonItems/OSCompatibilityLayer.h>
 #include <external/commonItems/external/googletest/googlemock/include/gmock/gmock-matchers.h>
 #include <external/commonItems/external/googletest/googletest/include/gtest/gtest.h>
 
-#include <ostream>
+#include <fstream>
 #include <sstream>
 
 #include "src/out_hoi4/decisions/out_decisions.h"
+
+
+
+using std::filesystem::create_directories;
+using std::filesystem::path;
+
+
+
+namespace
+{
+
+void CreateFolders(std::string_view test_name)
+{
+   const path test_path(test_name);
+   create_directories("output");
+   create_directories("output" / test_path);
+   create_directories("output" / test_path / "common");
+   create_directories("output" / test_path / "common/decisions");
+}
+
+}  // namespace
 
 
 namespace out
 {
 
 // Remove this test when decisions are implemented!
-TEST(OutHoI4DecisionsOutDecisions, UnimplementedFeatureIsLogged)
+TEST(OutHoI4DecisionsOutDecisions, DecisionsAreOutput)
 {
-   std::stringstream log;
-   std::streambuf* cout_buffer = std::cout.rdbuf();
-   std::cout.rdbuf(log.rdbuf());
-   OutputDecisions("dummy", {});
-   std::cout.rdbuf(cout_buffer);
-   EXPECT_THAT(log.str(), testing::HasSubstr("Decisions not implemented yet"));
+   const std::map<std::string, std::vector<hoi4::Decision>> decisions{
+       {"$TAG$_a_decisions_category",
+           std::vector<hoi4::Decision>{hoi4::Decision{.name = "$TAG$_a_decision"},
+               hoi4::Decision{.name = "$TAG$_another_decision"}}},
+       {"$TAG$_another_decisions_category",
+           std::vector<hoi4::Decision>{hoi4::Decision{.name = "$TAG$_a_third_decision"}}},
+   };
+
+   CreateFolders("DecisionsAreOutput");
+   OutputDecisions("DecisionsAreOutput", decisions);
+
+   const std::filesystem::path expected_file = "output/DecisionsAreOutput/common/decisions/stories_decisions.txt";
+   EXPECT_TRUE(commonItems::DoesFileExist(expected_file));
+   std::ifstream decisions_file(expected_file);
+   ASSERT_TRUE(decisions_file.is_open());
+   std::stringstream decisions_file_stream;
+   std::copy(std::istreambuf_iterator<char>(decisions_file),
+       std::istreambuf_iterator<char>(),
+       std::ostreambuf_iterator<char>(decisions_file_stream));
+   decisions_file.close();
+   EXPECT_EQ(decisions_file_stream.str(),
+       "$TAG$_a_decisions_category = {\n"
+       "\n"
+       "\n"
+       "\t$TAG$_a_decision = {\n"
+       "\t}\n"
+       "\n"
+       "\t$TAG$_another_decision = {\n"
+       "\t}\n"
+       "\n"
+       "}\n"
+       "\n"
+       "$TAG$_another_decisions_category = {\n"
+       "\n"
+       "\n"
+       "\t$TAG$_a_third_decision = {\n"
+       "\t}\n"
+       "\n"
+       "}\n"
+       "\n");
 }
 
 }  // namespace out
