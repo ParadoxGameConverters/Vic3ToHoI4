b6613a54c6333f6b0967d279615edfe70e707365
Idhrendur <idhrendur@hotmail.com>
2025-04-26 12:05:23 -0700

    Include the event option definition

---
+++ src/hoi4_world/events/event_option.h
@@ -0,0 +1,28 @@
+#ifndef SRC_HOI4WORLD_EVENT_OPTION_H
+#define SRC_HOI4WORLD_EVENT_OPTION_H
+
+
+#include <external/commonItems/Parser.h>
+
+#include <string>
+#include <vector>
+
+
+
+namespace hoi4
+{
+
+struct EventOption
+{
+   std::string name;
+   std::string trigger;
+   std::string ai_chance;
+   std::vector<std::string> script_blocks;
+   std::string hidden_effect;
+};
+
+}  // namespace hoi4
+
+
+
+#endif  // SRC_HOI4WORLD_EVENT_OPTION_H
