5d94dea135ff5e5494982292035b64d2cf4dca2b
Idhrendur <idhrendur@hotmail.com>
2025-09-01 21:12:20 -0700

    Add FindAllValid()

--- src/hoi4_world/roles/triggers/always_trigger.h
+++ src/hoi4_world/roles/triggers/always_trigger.h
@@ -31,6 +31,11 @@ class AlwaysTrigger: public Trigger
    {
       return value_;
    }
+   [[nodiscard]] std::vector<Scope> FindAllValid([[maybe_unused]] const Context& context,
+       [[maybe_unused]] const World& world) const override
+   {
+      return {};
+   }
 
    // This allows the Google test framework to print human-readable RepeatFocuses if a test fails.
    friend void PrintTo(const AlwaysTrigger& trigger, std::ostream* os)
--- src/hoi4_world/roles/triggers/nand_trigger_tests.cpp
+++ src/hoi4_world/roles/triggers/nand_trigger_tests.cpp
@@ -50,6 +50,25 @@ TEST(Hoi4worldRolesTriggersNandTriggerTests, IsValidReturnsTrueIfAnyChildrenFals
 }
 
 
+TEST(Hoi4worldRolesTriggersNandTriggerTests, FindAllValidReturnsEmptyVector)
+{
+   std::unique_ptr<Trigger> trigger_one = std::make_unique<AlwaysTrigger>(true);
+   std::unique_ptr<Trigger> trigger_two = std::make_unique<AlwaysTrigger>(false);
+
+   std::vector<std::unique_ptr<Trigger>> children;
+   children.push_back(std::move(trigger_one));
+   children.push_back(std::move(trigger_two));
+   const NandTrigger nand_trigger(std::move(children));
+
+   const State state(1, {});
+   const Scope scope = StateScope{.state = state};
+   const Context context{.root = scope, .this_scope = scope, .prev = scope, .from = scope};
+   const hoi4::World world({});
+
+   EXPECT_TRUE(nand_trigger.FindAllValid(context, world).empty());
+}
+
+
 TEST(Hoi4worldRolesTriggersNandTriggerTests, EquivalentTriggersAreEqual)
 {
    std::unique_ptr<Trigger> true_trigger_one = std::make_unique<AlwaysTrigger>(true);
