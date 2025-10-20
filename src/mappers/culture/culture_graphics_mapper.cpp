1705e8acfcf36dd42edad0e7aa59097054de1f04
Idhrendur <idhrendur@hotmail.com>
2025-10-19 19:41:57 -0700

    Formatting

--- src/mappers/culture/culture_graphics_mapper.cpp
+++ src/mappers/culture/culture_graphics_mapper.cpp
@@ -86,8 +86,7 @@ void EmplaceUnitGraphics(std::map<std::string, std::string>& unit_graphics, cons
 namespace mappers
 {
 
-GraphicsBlock CultureGraphicsMapper::MatchPrimaryCulturesToGraphics(
-    const std::set<std::string>& primary_cultures,
+GraphicsBlock CultureGraphicsMapper::MatchPrimaryCulturesToGraphics(const std::set<std::string>& primary_cultures,
     const std::map<std::string, vic3::CultureDefinition>& cultures) const
 {
    GraphicsBlock graphics_block;
