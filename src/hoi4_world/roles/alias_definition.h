94ab186f99b48b5d98748263eb35e9d5a2a2de77
Idhrendur <idhrendur@hotmail.com>
2025-12-14 17:48:37 -0800

    Build fix

--- src/hoi4_world/roles/alias_definition.h
+++ src/hoi4_world/roles/alias_definition.h
@@ -13,6 +13,8 @@ struct AliasDefinition
 {
    std::string country_flag;
    std::string alias_identifier;
+
+   auto operator<=>(const AliasDefinition&) const = default;
 };
 
 }  // namespace hoi4
