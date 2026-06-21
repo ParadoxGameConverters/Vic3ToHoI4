18fd2f0718c7090982850372f079cb51f7bf7dec
Idhrendur <idhrendur@hotmail.com>
2026-06-21 13:57:29 -0700

    Formatting

--- src/hoi4_world/roles/triggers/country_has_primary_culture_trigger.cpp
+++ src/hoi4_world/roles/triggers/country_has_primary_culture_trigger.cpp
@@ -13,14 +13,14 @@ bool CountryHasPrimaryCultureTrigger::IsValid(const Context& context, [[maybe_un
       return false;
    }
 
-    if (culture_.starts_with("cu:"))
-    {
-        return maybe_country->country.GetPrimaryCultures().contains(culture_.substr(3, culture_.length()));
-    }
-    else
-    {
-        return maybe_country->country.GetPrimaryCultures().contains(culture_);
-    }
+   if (culture_.starts_with("cu:"))
+   {
+      return maybe_country->country.GetPrimaryCultures().contains(culture_.substr(3, culture_.length()));
+   }
+   else
+   {
+      return maybe_country->country.GetPrimaryCultures().contains(culture_);
+   }
 }
 
 
